74b0008863ce61d4e91bf9e7564cf4ba2df4c4af
frank <frankode254@gmail.com>
2024-04-22 00:04:47 +0300

    add3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,29 @@
-include stdlib
+# Puppet manifest to configure SSH client for passwordless authentication
 
-file_line { 'SSH Private Key':
-  path               => '/etc/ssh/ssh_config',
-  line               => '    IdentityFile ~/.ssh/school',
-  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
-  replace            => true,
-  append_on_no_match => true
+# Ensure SSH directory exists
+file { '/etc/ssh':
+  ensure => directory,
 }
 
-file_line { 'Deny Password Auth':
+# Configure SSH client to use private key for authentication
+file_line { 'Declare identity file':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^(\s*)IdentityFile\s.*',
+  append_on_no_match => true,
+}
+
+# Configure SSH client to refuse password authentication
+file_line { 'Turn off passwd auth':
   path               => '/etc/ssh/ssh_config',
   line               => '    PasswordAuthentication no',
-  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
-  replace            => true,
-  append_on_no_match => true
+  match              => '^(\s*)PasswordAuthentication\s.*',
+  append_on_no_match => true,
+}
+
+# Notify SSH service to reload configuration
+service { 'ssh':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/ssh/ssh_config'],
 }
